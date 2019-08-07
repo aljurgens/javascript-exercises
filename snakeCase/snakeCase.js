146168bc5766686395bfb0ed060361d86ea5b056
Alyssa Jurgens <aljurgens@yahoo.com>
2019-08-07 17:48:14 -0600

    Update snakeCase exercise

---
+++ snakeCase/snakeCase.js
@@ -0,0 +1,15 @@
+const snakeCase = function(str) {
+  str = str.split('..').join(' ');
+  str = str.toLowerCase().replace(/[^\w\s-]|_/g, '').replace(/[\s-]+/g, '_');
+
+  // to remove trailing '_'
+  let lastChar = str[str.length - 1];
+  while (lastChar == '_') {
+    str = str.substring(0, str.length - 1);
+    lastChar = str[str.length - 1];
+  }
+
+  return str;
+}
+
+module.exports = snakeCase
