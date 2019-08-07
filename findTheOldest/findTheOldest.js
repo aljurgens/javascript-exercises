4e87ed190e87184f6bb9574f1b4529befe2ba3b9
Alyssa Jurgens <aljurgens@yahoo.com>
2019-08-07 17:46:21 -0600

    Complete findTheOldest exercise

---
+++ findTheOldest/findTheOldest.js
@@ -0,0 +1,21 @@
+let findTheOldest = function(people) {
+  let oldest = 0;
+
+  for (let i = 0; i < people.length; i++) {
+    // make sure still-living person's current ages are properly computed
+    if (people[i].yearOfDeath == undefined) {
+      people[i].yearOfDeath = new Date().getFullYear();
+    }
+
+    let personsAge = people[i].yearOfDeath - people[i].yearOfBirth;
+    let oldestsAge = people[oldest].yearOfDeath - people[oldest].yearOfBirth;
+
+    if (personsAge > oldestsAge) {
+      oldest = i;
+    }
+  }
+
+  return people[oldest];
+}
+
+module.exports = findTheOldest
